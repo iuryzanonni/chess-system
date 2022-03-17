73220bb22ba142730c53b5607df643a2c5ba7029
Iury Zanonni <izanonni@gmail.com>
2022-03-17 02:39:37 -0300

    Created ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,10 +26,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(this.board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(this.board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(this.board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(this.board, Color.WHITE));
+		placeNewPiece('e', 8, new King(this.board, Color.BLACK));
+		placeNewPiece('e', 1, new King(this.board, Color.WHITE));
 	}
 
 }
